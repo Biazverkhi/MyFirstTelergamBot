78868476a34d6a6aea01a7c67c5ceae0d2024ac8
wertual <wertual@gmail.com>
2020-01-15 12:42:43 +0300

    changed the Java versions to 8

--- rest/src/main/java/com/github/biazverkhi/bot/rest/config/RestConfig.java
+++ rest/src/main/java/com/github/biazverkhi/bot/rest/config/RestConfig.java
@@ -4,10 +4,8 @@ import com.github.biazverkhi.bot.rest.controller.CityRestController;
 import com.github.biazverkhi.bot.service.config.ServiceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class RestConfig {
 
     private ServiceConfig serviceConfig;
